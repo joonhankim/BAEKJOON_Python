e5263daab5a1589effaaf8b815e25fcacd67097a
김준한 <kimjoonhan@eric.local>
2022-04-24 18:00:12 +0900

    code update

--- 브루트 포스/#2231.py
+++ 브루트 포스/#2231.py
@@ -1,10 +1,10 @@
 n = int(input())
 
 for i in range(1,n+1):
-    num = sum(map(int,str(i)))
-    num_sum = i + num
+    num = sum(map(int,str(i))) #각 자리수 더함
+    num_sum = i + num #분해합
     if num_sum == n:
         print(i)
         break
-    if i == n:
+    if i == n: #생성자가 없는 경우
         print(0)
