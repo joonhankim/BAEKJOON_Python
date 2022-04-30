1025bc385ade3ae3940b69c942d02692c02f3b6a
김준한 <kimjoonhan@eric.local>
2022-04-30 16:08:51 +0900

    code update

--- 동적 계획법 1/#9184.py
+++ 동적 계획법 1/#9184.py
@@ -1,31 +1,25 @@
 # 재귀함수를 사용하면 시간이 너무 오래걸림
 # 동적 계획법을 사용해야함 (메모라이징)
-#계산한 값을 중간에 확인하는 분기를 주면, 불필요한 계산을 줄이고 이전에 계산한 값을 사용할 수 있음.
-
-import sys
-
-cache = {}
+dp = [[[0 for _ in range(21)]for _ in range(21)]for _ in range(21)]
 
 def w(a,b,c):
+    
     if a <= 0 or b <= 0 or c <= 0:
         return 1
-    if a > 20 or b > 20 or c > 20:
+    elif a > 20 or b >20 or c > 20:
         return w(20,20,20)
-    # if a<b<c:
-    #     return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
 
-    key = '{} {} {}'.format(a, b, c)
-
-    #계산한 값을 중간에 확인하는 분기 (메모라이징)
-    if key in cache:
-        return cache[key]
-    res = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-    cache[key] = res
-
-    return res
+    if dp[a][b][c] != 0:
+        return dp[a][b][c]
+    elif a < b and b < c:
+        dp[a][b][c] = (w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c))
+        return dp[a][b][c]
+    else:
+        dp[a][b][c] = (w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1))
+        return dp[a][b][c]
 
 while True:
-    a,b,c = map(int,sys.stdin.readline().split())
-    if a == -1 and b == -1 and c == -1:
+    a,b,c = map(int,(input().split()))
+    if a == -1 and b == -1 and c== -1:
         break
-    print('w({}, {}, {}) = {}'.format(a, b, c, w(a, b, c)))
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
