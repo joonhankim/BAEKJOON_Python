ea8fd193cf412af88f286d92df08b3b74bb813e5
김준한 <kimjoonhan@eric.local>
2022-05-01 21:53:05 +0900

    sunday code update

--- 그리디 알고리즘/#1541.py
+++ 그리디 알고리즘/#1541.py
@@ -1,11 +1,10 @@
 #-와 - 사이의 수를 전부 괄호치면 끝!!!
 
 arr = input().split('-') 
-s = 0 
+ans = 0 
 for i in arr[0].split('+'): 
-    s += int(i) 
+    ans += int(i) 
 for i in arr[1:]: 
     for j in i.split('+'): 
-        s -= int(j) 
-print(s)
-
+        ans -= int(j) 
+print(ans)
