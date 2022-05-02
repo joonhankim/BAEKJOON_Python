11542cc9f6075be603626e99e8869dd42c81ce73
김준한 <kimjoonhan@eric.local>
2022-05-03 02:41:02 +0900

    monday code update

--- 이분 탐색/#1654.py
+++ 이분 탐색/#1654.py
@@ -7,20 +7,23 @@ lans = [int(sys.stdin.readline()) for _ in range(K)]
 
 start, end = 1, max(lans)
 
-def binarySearch(start, end, N):
+def binary_search(start, end, N):
     if start > end:
         return end
     
     mid = (start + end) // 2
 
     lines = 0
+
+    ####이게 핵심############
     for lan in lans:
         lines += lan // mid
-    
-    if lines >= N:
-        return binarySearch(mid+1, end, N)
-    else:
-        return binarySearch(start, mid-1, N)
+    ######################
 
-print(binarySearch(start, end, N))
+    if lines >= N:
+        return binary_search(mid+1, end, N)
+    else:
+        return binary_search(start, mid-1, N)
+
+print(binary_search(start, end, N))
 
