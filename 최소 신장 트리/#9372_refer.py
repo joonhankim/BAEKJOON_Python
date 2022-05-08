27c90074c3a47fab34c43cb2b4e79947fe73c10f
김준한 <kimjoonhan@eric.local>
2022-05-08 20:56:10 +0900

    sunday code update

--- 최소 신장 트리/#9372_refer.py
+++ 최소 신장 트리/#9372_refer.py
@@ -1,8 +1,9 @@
 import sys
 
-for t in range(int(input())):
-    N, M = map(int, input().split())
-    for i in range(M):
-        a, b = map(int, sys.stdin.readline().split())
+if __name__ == '__main__':
+    for t in range(int(input())):
+        N, M = map(int, input().split())
+        for i in range(M):
+            a, b = map(int, sys.stdin.readline().split())
 
-    print(N-1)
+        print(N-1)
